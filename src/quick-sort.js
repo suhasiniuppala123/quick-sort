df50a35833eedf59d8eed80a685680e88a4072d1
Suhasini Uppala <Suhasini_Uppala@epam.com>
2017-07-12 15:40:34 +0530

    quicksort implementation

--- src/quick-sort.js
+++ src/quick-sort.js
@@ -1,6 +1,29 @@
+
 'use strict';
 
 function quickSort(input) {
 
+   if (input.length <= 1) { 
+		return input;
+	} else {
+
+		var left = [];
+		var right = [];
+		var newArray = [];
+		var pivot = input.pop();
+		var length = input.length;
+
+		for (var i = 0; i < length; i++) {
+			if (input[i] <= pivot) {
+				left.push(input[i]);
+			} else {
+				right.push(input[i]);
+			}
+		}
+
+		return newArray.concat(quickSort(left), pivot, quickSort(right));
+	} 
+    
 };
 module.exports = quickSort;
+
